82d25c29eb2cfaadcce45883e55660d3989eb1d8
Mukisa Vaniah Christian <vierycalliper@gmail.com>
2026-07-13 14:35:19 +0300

    fix deployment

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { Sidebar } from "@/components/sidebar";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardLayout({
   children,
 }: {
