aa66ab2e2d5700f297be3597e151fdfb0c4e828a
Ssemaganda-George <ssemagandageorge480@gmail.com>
2026-07-16 09:53:54 +0300

    new

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default async function DashboardLayout({
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="flex-1 overflow-y-auto">
+      <main className="flex-1 overflow-y-auto pt-14 lg:pt-0">
         <div className="max-w-6xl mx-auto px-4 py-4 lg:px-8 lg:py-8">
           {children}
         </div>
